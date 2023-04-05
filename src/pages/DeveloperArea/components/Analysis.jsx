f55116345be8c36ace5109dbbdda73640fbdb767
ugur <u9urturk@gmail.com>
2023-04-05 12:52:36 +0300

    Refactoring

--- src/pages/DeveloperArea/components/Analysis.jsx
+++ src/pages/DeveloperArea/components/Analysis.jsx
@@ -1,33 +1,106 @@
-import React, { useState } from 'react'
+import { current } from '@reduxjs/toolkit'
+import React, { useEffect, useRef, useState } from 'react'
 
 export default function Analysis() {
     const imagesUrl = process.env.PUBLIC_URL + `/SDTG/`
+    const ref = useRef()
     const [isOpen, setisOpen] = useState(false)
+    const [res,setRes] = useState({
+        gender:null,
+        age:null,
+        zx:null,
+        zy:null,
+        xy:null,
+        xx:null
+
+    })
+
+    useEffect(() => {
+     console.log(res)
+    }, [res])
+    
+
+    const test = ()=>{
+        setRes({gender:"1",age:"22",xx:"test"})
+    }
+
+    const deneme = (gender)=>{
+        setRes({gender:1})
+        // 1 => Man
+        // 2 => Women
+        if(gender===1){
+            let key = 1
+            switch (key=1) {
+                case 1:
+                    //age
+
+                    let age = ref.current.querySelector("#age")
+                    age.classList.remove('hidden')
+                    console.log(age)
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    }
+     
+
+    const ages = [
+        {
+            id:1,
+            age:"18-25"
+
+        },
+        {
+            id:2,
+            age:"26-35"
+
+        },
+        {
+            id:3,
+            age:"36-45"
+
+        },
+        {
+            id:4,
+            age:"46-65"
+
+        },
+        {
+            id:5,
+            age:"65+"
+        }
+        
+    ]
+
     const modalActive = () => {
         if (isOpen) {
             return (
-                <div className='fixed left-0 top-0 w-full h-screen backdrop-blur-sm flex items-center justify-center'>
+                <div  className='fixed left-0 top-0 w-full h-screen backdrop-blur-sm flex items-center justify-center'>
 
-                    <div onClick={() => { setisOpen(false) }} className='absolute cursor-pointer text-2xl font-bold right-8 top-8'>X</div>
+                    <div onClick={() => {deneme(1) }} className='absolute cursor-pointer text-2xl font-bold right-8 top-8'>X</div>
 
                     <div>
-                        <div className='flex flex-col gap-y-4'>
-                            <ul className='hidden flex flex-col px-4 py-6 rounded-md bg-gray-200 gap-y-2  items-center justify-center h-auto max-w-4xl w-[200px]'>
+                        <div ref={ref} className='flex flex-col gap-y-4'>
+                            <ul id='age' className='hidden flex flex-col px-4 py-6 rounded-md bg-gray-200 gap-y-2  items-center justify-center h-auto max-w-4xl w-[200px]'>
                                 <strong>Yaş</strong>
-                                <li className='hover:bg-brand-color w-full h-auto flex items-center justify-center cursor-pointer transition-all rounded-md active:scale-95'>18 - 25</li>
-                                <li className='hover:bg-brand-color w-full h-auto flex items-center justify-center cursor-pointer transition-all rounded-md active:scale-95'>26 - 35</li>
-                                <li className='hover:bg-brand-color w-full h-auto flex items-center justify-center cursor-pointer transition-all rounded-md active:scale-95'>36 - 45</li>
-                                <li className='hover:bg-brand-color w-full h-auto flex items-center justify-center cursor-pointer transition-all rounded-md active:scale-95'>46 - 65</li>
-                                <li className='hover:bg-brand-color w-full h-auto flex items-center justify-center cursor-pointer transition-all rounded-md active:scale-95'> 65+ </li>
+                                {
+                                    ages.map(age=>{
+                                      return  <li key={age.id} onClick={()=>{setRes({age:age.age})}} className='hover:bg-brand-color w-full h-auto flex items-center justify-center cursor-pointer transition-all rounded-md active:scale-95'>{age.age}</li>
+                                    })
+                                }
+                                
                             </ul>
-                            <div className='hidden flex items-center justify-center flex-col bg-gray-200 rounded-md px-4 py-6 gap-y-2'>
+                            <div id='zx' className='hidden flex items-center justify-center flex-col bg-gray-200 rounded-md px-4 py-6 gap-y-2'>
                                 <strong>Ailenizde saçı dökülen var mı ?</strong>
                                 <div className='flex items-center justify-center gap-x-6 '>
                                     <button className='bg-white h-auto w-24 rounded-sm hover:bg-brand-color transition-all active:scale-95'>Evet</button>
                                     <button className='bg-white h-auto w-24 rounded-sm hover:bg-brand-color transition-all active:scale-95'>Hayır</button>
                                 </div>
                             </div>
-                            <ul className='flex hidden flex-col rounded-sm items-center justify-center px-4 py-6 bg-gray-200 gap-y-2'>
+                            <ul id='zy' className='flex hidden flex-col rounded-sm items-center justify-center px-4 py-6 bg-gray-200 gap-y-2'>
                                 <strong>Kaç Yıldır Saçınız Dökülüyor ? </strong>
                                 <li className='hover:bg-brand-color w-full h-auto flex items-center justify-center cursor-pointer transition-all rounded-md active:scale-95'>1</li>
                                 <li  className='hover:bg-brand-color w-full h-auto flex items-center justify-center cursor-pointer transition-all rounded-md active:scale-95'>2</li>                                
@@ -40,7 +113,7 @@ export default function Analysis() {
                                 <li  className='hover:bg-brand-color w-full h-auto flex items-center justify-center cursor-pointer transition-all rounded-md active:scale-95'>9</li>
                                 <li  className='hover:bg-brand-color w-full h-auto flex items-center justify-center cursor-pointer transition-all rounded-md active:scale-95'>10+</li>
                             </ul>
-                            <ul className='flex flex-col rounded-md  items-center justify-center  px-4 py-6 bg-gray-200 gap-y-4'>
+                            <ul id='xy' className=' hidden flex-col rounded-md  items-center justify-center  px-4 py-6 bg-gray-200 gap-y-4'>
                                 <strong>Saçınızın Dökülme Tipini Seçin</strong>
                                 <div className='grid grid-cols-2 grid-flow-row gap-4'>
                                     <img src={imagesUrl+"erkekderece1.png"} className='border-[1px] h-auto w-48 hover:border-white cursor-pointer active:scale-95 ' alt="" />                                   
@@ -50,7 +123,7 @@ export default function Analysis() {
 
                                 </div>
                             </ul>
-                            <ul className='flex flex-col rounded-md  items-center justify-center  px-4 py-6 bg-gray-200 gap-y-4'>
+                            <ul id='xx' className=' hidden flex flex-col rounded-md   items-center justify-center  px-4 py-6 bg-gray-200 gap-y-4'>
                                 <strong>Saçınızın Dökülme Tipini Seçin</strong>
                                 <div className='grid grid-cols-2 grid-flow-row gap-4'>
                                     <img src={imagesUrl+"kadinderece1.png"} className='border-[1px] h-auto w-48 hover:border-white cursor-pointer active:scale-95 ' alt="" />                                   
