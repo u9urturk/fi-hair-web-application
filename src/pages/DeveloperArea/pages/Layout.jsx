8a55fcc6a794585aad884f3e9cb51bf67345e7cb
ugur <u9urturk@gmail.com>
2023-06-01 17:50:24 +0300

    Refactoring

--- src/pages/DeveloperArea/pages/Layout.jsx
+++ src/pages/DeveloperArea/pages/Layout.jsx
@@ -1,10 +1,4 @@
-import React, { useState } from 'react'
-import { AiOutlineFacebook } from 'react-icons/ai'
-import { BsInstagram } from 'react-icons/bs'
-import { FiTwitter } from 'react-icons/fi'
-import{MdForwardToInbox}from 'react-icons/md'
-import store from '../../../store'
-import { logout } from '../../../store/auth'
+import React from 'react'
 import Analysis from '../components/Analysis'
 import Communication from '../components/Communication'
 import SliderCmp from '../components/SliderCmp'
@@ -12,10 +6,6 @@ import Welcome from '../components/Welcome'
 import FemaleType from '../components/FemaleType'
 import BeforeAfter from '../components/BeforeAfter'
 import WhyFihair from '../components/WhyFihair'
-import Inbox from '../components/inbox.jsx'
-import { useSelector } from 'react-redux'
-
-
 
 
 export default function Layout() {
